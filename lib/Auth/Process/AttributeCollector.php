4d37d7a2297dcf12ee0e1804f99afb0e86a8b039
Frank Tamás <sitya@niif.hu>
2018-09-07 09:32:43 +0200

    PSR-2 coding style

--- lib/Auth/Process/AttributeCollector.php
+++ lib/Auth/Process/AttributeCollector.php
@@ -3,81 +3,81 @@
 /**
  * Filter to collect attributes from diferent sources.
  */
-class sspmod_attributecollector_Auth_Process_AttributeCollector extends SimpleSAML_Auth_ProcessingFilter {
-
-	private $existing = 'ignore';
-	private $collector = NULL;
-	private $uidfield = NULL;
-
-
-	/**
-	 * Get and initialize the configured collector
-	 *
-	 * @param array $config	 Configuration information about this filter.
-	 */
-	private function getCollector($config) {
-		if (!array_key_exists("collector", $config) || !array_key_exists("class", $config["collector"])) {
-			throw new Exception('No collector class specified in configuration');
-		}
-		$collectorConfig = $config["collector"];
-		$collectorClassName = SimpleSAML\Module::resolveClass($collectorConfig['class'], 'Collector', 'sspmod_attributecollector_SimpleCollector');
-		unset($collectorConfig['class']);
-		return new $collectorClassName($collectorConfig);
-	}
-
-	/**
-	 * Initialize this filter.
-	 *
-	 * @param array $config	 Configuration information about this filter.
-	 * @param mixed $reserved  For future use.
-	 */
-	public function __construct($config, $reserved) {
-		parent::__construct($config, $reserved);
-
-		assert('is_array($config)');
-
-		if (!array_key_exists("uidfield", $config)) {
-			throw new Exception('No uidfield specified in configuration');
-		}
-		$this->uidfield = $config["uidfield"];
-		$this->collector = $this->getCollector($config);
-		if (array_key_exists("existing", $config)) {
-			$this->existing = $config["existing"];
-		}
-	}
+class sspmod_attributecollector_Auth_Process_AttributeCollector extends SimpleSAML_Auth_ProcessingFilter
+{
+    private $existing = 'ignore';
+    private $collector = null;
+    private $uidfield = null;
 
 
-	/**
-	 * Apply filter expand attributes with collected ones
-	 *
-	 * @param array &$request  The current request
-	 */
-	public function process(&$request) {
-		assert('is_array($request)');
-		assert('array_key_exists("Attributes", $request)');
+    /**
+     * Get and initialize the configured collector
+     *
+     * @param array $config  Configuration information about this filter.
+     */
+    private function getCollector($config)
+    {
+        if (!array_key_exists("collector", $config) || !array_key_exists("class", $config["collector"])) {
+            throw new Exception('No collector class specified in configuration');
+        }
+        $collectorConfig = $config["collector"];
+        $collectorClassName = SimpleSAML\Module::resolveClass($collectorConfig['class'], 'Collector', 'sspmod_attributecollector_SimpleCollector');
+        unset($collectorConfig['class']);
+        return new $collectorClassName($collectorConfig);
+    }
 
-		if (array_key_exists($this->uidfield, $request['Attributes'])) {
+    /**
+     * Initialize this filter.
+     *
+     * @param array $config  Configuration information about this filter.
+     * @param mixed $reserved  For future use.
+     */
+    public function __construct($config, $reserved)
+    {
+        parent::__construct($config, $reserved);
 
-			$newAttributes = $this->collector->getAttributes($request['Attributes'], $this->uidfield);
+        assert('is_array($config)');
 
-			if (is_array($newAttributes)) {
-				$attributes =& $request['Attributes'];
+        if (!array_key_exists("uidfield", $config)) {
+            throw new Exception('No uidfield specified in configuration');
+        }
+        $this->uidfield = $config["uidfield"];
+        $this->collector = $this->getCollector($config);
+        if (array_key_exists("existing", $config)) {
+            $this->existing = $config["existing"];
+        }
+    }
 
-				foreach($newAttributes as $name => $values) {
-					if (!is_array($values)) {
-						$values = array($values);
-					}
-					if (!array_key_exists($name, $attributes) || $this->existing === 'replace') {
-						$attributes[$name] = $values;
-					} else {
-						if ($this->existing === 'merge') {
-							$attributes[$name] = array_merge($attributes[$name], $values);
-						}
-					}
-				}
-			}
-		}
-	}
+
+    /**
+     * Apply filter expand attributes with collected ones
+     *
+     * @param array &$request  The current request
+     */
+    public function process(&$request)
+    {
+        assert('is_array($request)');
+        assert('array_key_exists("Attributes", $request)');
+
+        if (array_key_exists($this->uidfield, $request['Attributes'])) {
+            $newAttributes = $this->collector->getAttributes($request['Attributes'], $this->uidfield);
+
+            if (is_array($newAttributes)) {
+                $attributes =& $request['Attributes'];
+
+                foreach ($newAttributes as $name => $values) {
+                    if (!is_array($values)) {
+                        $values = array($values);
+                    }
+                    if (!array_key_exists($name, $attributes) || $this->existing === 'replace') {
+                        $attributes[$name] = $values;
+                    } else {
+                        if ($this->existing === 'merge') {
+                            $attributes[$name] = array_merge($attributes[$name], $values);
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
-
-?>
--- lib/Collector/SQLCollector.php
+++ lib/Collector/SQLCollector.php
@@ -8,25 +8,25 @@
  *
  * It has the following options:
  * - dsn: The DSN which should be used to connect to the database server. Check the various
- *		  database drivers in http://php.net/manual/en/pdo.drivers.php for a description of
- *		  the various DSN formats.
+ *        database drivers in http://php.net/manual/en/pdo.drivers.php for a description of
+ *        the various DSN formats.
  * - username: The username which should be used when connecting to the database server.
  * - password: The password which should be used when connecting to the database server.
  * - query: The sql query for retrieve attributes. You can use the special :uidfield string
- *			to refer the value of the field especified as an uidfield in the processor.
+ *          to refer the value of the field especified as an uidfield in the processor.
  *
  *
  * Example - with PostgreSQL database:
  * <code>
  * 'collector' => array(
- *		 'class' => 'attributecollector:SQLCollector',
- *		 'dsn' => 'pgsql:host=localhost;dbname=simplesaml',
- *		 'username' => 'simplesaml',
- *		 'password' => 'secretpassword',
- *		 'query' => 'select address, phone, country from extraattributes where uid=:uidfield',
- *		 ),
- *	   ),
- *	 ),
+ *       'class' => 'attributecollector:SQLCollector',
+ *       'dsn' => 'pgsql:host=localhost;dbname=simplesaml',
+ *       'username' => 'simplesaml',
+ *       'password' => 'secretpassword',
+ *       'query' => 'select address, phone, country from extraattributes where uid=:uidfield',
+ *       ),
+ *     ),
+ *   ),
  * </code>
  *
  * SQLCollector allows to specify several database connections which will
@@ -47,261 +47,258 @@
  *  ),
  */
 
-class sspmod_attributecollector_Collector_SQLCOllector extends sspmod_attributecollector_SimpleCollector {
+class sspmod_attributecollector_Collector_SQLCOllector extends sspmod_attributecollector_SimpleCollector
+{
 
 
-	/**
-	 * DSN for the database.
-	 */
-	private $dsn;
+    /**
+     * DSN for the database.
+     */
+    private $dsn;
 
 
-	/**
-	 * Username for the database.
-	 */
-	private $username;
+    /**
+     * Username for the database.
+     */
+    private $username;
 
 
-	/**
-	 * Password for the database;
-	 */
-	private $password;
+    /**
+     * Password for the database;
+     */
+    private $password;
 
 
-	/**
-	 * Query for retrieving attributes
-	 */
-	private $query;
+    /**
+     * Query for retrieving attributes
+     */
+    private $query;
 
 
-	/**
-	 * Query for retrieving all entries
-	 */
-	private $getAllQuery;
+    /**
+     * Query for retrieving all entries
+     */
+    private $getAllQuery;
 
-	/**
-	 * Valid connection
-	 */
-	private $current;
+    /**
+     * Valid connection
+     */
+    private $current;
 
-	/**
-	 * Total configured databases
-	 */
-	private $total;
-
-
-	/**
-	 * Database handle.
-	 *
-	 * This variable can't be serialized.
-	 */
-	private $db;
+    /**
+     * Total configured databases
+     */
+    private $total;
 
 
-	/**
-	 * Attribute name case.
-	 *
-	 * This is optional and by default is "natural"
-	 */
-	private $attrcase;
+    /**
+     * Database handle.
+     *
+     * This variable can't be serialized.
+     */
+    private $db;
 
 
-	/* Initialize this collector.
-	 *
-	 * @param array $config  Configuration information about this collector.
-	 */
-	public function __construct($config) {
-		$this->total = 0;
-		$this->current = 0;
-
-		foreach (array('dsn', 'username', 'password', 'query', 'get_all_query') as $id) {
-			if (!array_key_exists($id, $config)) {
-				throw new Exception('attributecollector:SQLCollector - Missing required option \'' . $id . '\'.');
-			}
-
-			if (is_array($config[$id])) {
-
-				// Check array size
-				if ($this->total == 0) {
-					$this->total = count($config[$id]);
-				} elseif (count($config[$id]) != $this->total) {
-					throw new Exception('attributecollector:SQLCollector - \'' . $id . '\' size != ' . $this->total);
-				}
-
-			} elseif (is_string($config[$id])) {
-				// TODO: allow single values
-				// when using arrays on previous fields?
-				if ($this->total > 1) {
-					throw new Exception('attributecollector:SQLCollector - \'' . $id . '\' is supposed to be an array.');
-				}
-
-				$config[$id] = array($config[$id]);
-				$this->total = 1;
-			} else {
-				throw new Exception('attributecollector:SQLCollector - \'' . $id . '\' is supposed to be a string or array.');
-			}
-		}
-
-		$this->dsn = $config['dsn'];
-		$this->username = $config['username'];
-		$this->password = $config['password'];
-		$this->query = $config['query'];
-		$this->getAllQuery = $config['get_all_query'];
-		$this->current = 0;
-
-		$case_options = array ("lower" => PDO::CASE_LOWER,
-			"natural" => PDO::CASE_NATURAL,
-			"upper" => PDO::CASE_UPPER);
-		// Default is 'natural'
-		$this->attrcase = $case_options["natural"];
-		if (array_key_exists("attrcase", $config)) {
-			$attrcase = $config["attrcase"];
-			if (in_array($attrcase, array_keys($case_options))) {
-				$this->attrcase = $case_options[$attrcase];
-			} else {
-				throw new Exception("attributecollector:SQLCollector - Wrong case value: '" . $attrcase . "'");
-			}
-		}
-	}
+    /**
+     * Attribute name case.
+     *
+     * This is optional and by default is "natural"
+     */
+    private $attrcase;
 
 
-	/* Get collected attributes
-	 *
-	 * @param array $originalAttributes      Original attributes existing before this collector has been called
-	 * @param string $uidfield      Name of the field used as uid
-	 * @return array  Attributes collected
-	 */
-	public function getAttributes($originalAttributes, $uidfield) {
-		assert('array_key_exists($uidfield, $originalAttributes)');
-		$db = $this->getDB();
-		$st = $db->prepare($this->query[$this->current]);
-		if (FALSE === $st) {
-			$err = $st->errorInfo();
-			$err_msg = 'attributecollector:SQLCollector - invalid query';
-			if (isset($err[2])) {
-				$err_msg .= ': '.$err[2];
-			}
-			throw new SimpleSAML_Error_Exception('attributecollector:SQLCollector - invalid query: '.$err[2]);
-		}
+    /* Initialize this collector.
+     *
+     * @param array $config  Configuration information about this collector.
+     */
+    public function __construct($config)
+    {
+        $this->total = 0;
+        $this->current = 0;
 
-		$res = $st->execute(array(':uidfield' => $originalAttributes[$uidfield][0]));
+        foreach (array('dsn', 'username', 'password', 'query', 'get_all_query') as $id) {
+            if (!array_key_exists($id, $config)) {
+                throw new Exception('attributecollector:SQLCollector - Missing required option \'' . $id . '\'.');
+            }
 
-		if (FALSE === $res){
-			$err = $st->errorInfo();
-			$err_msg = 'attributecollector:SQLCollector - invalid query execution';
+            if (is_array($config[$id])) {
+                // Check array size
+                if ($this->total == 0) {
+                    $this->total = count($config[$id]);
+                } elseif (count($config[$id]) != $this->total) {
+                    throw new Exception('attributecollector:SQLCollector - \'' . $id . '\' size != ' . $this->total);
+                }
+            } elseif (is_string($config[$id])) {
+                // TODO: allow single values
+                // when using arrays on previous fields?
+                if ($this->total > 1) {
+                    throw new Exception('attributecollector:SQLCollector - \'' . $id . '\' is supposed to be an array.');
+                }
 
-			if (isset($err[2])) {
-				$err_msg .= ': '.$err[2];
-			}
-			else if (isset($err[0])) {
-				$err_msg .= ': SQLSTATE['.$err[0].']';
-			}
-			throw new SimpleSAML_Error_Exception($err_msg);
-		}
+                $config[$id] = array($config[$id]);
+                $this->total = 1;
+            } else {
+                throw new Exception('attributecollector:SQLCollector - \'' . $id . '\' is supposed to be a string or array.');
+            }
+        }
 
-		$db_res = $st->fetchAll(PDO::FETCH_ASSOC);
+        $this->dsn = $config['dsn'];
+        $this->username = $config['username'];
+        $this->password = $config['password'];
+        $this->query = $config['query'];
+        $this->getAllQuery = $config['get_all_query'];
+        $this->current = 0;
 
-		$result = array();
-		foreach($db_res as $tuple) {
-			foreach($tuple as $colum => $value) {
-				$result[$colum][] = $value;
-			}
-		}
-		foreach($result as $colum => $data) {
-			$result[$colum] = array_unique($data);
-		}
-
-		return $result;
-	}
-
-
-	/**
-	 * Get database handle.
-	 *
-	 * @return PDO|FALSE  Database handle, or FALSE if we fail to connect.
-	 */
-	private function getDB() {
-		if ($this->db !== NULL) {
-			return $this->db;
-		}
-
-		for ($i = 0; $i<$this->total; $i++) {
-			$this->current = $i;
-			try {
-				$this->db = new PDO($this->dsn[$i], $this->username[$i], $this->password[$i]);
-			} catch (PDOException $e) {
-				SimpleSAML\Logger::error('attributecollector:SQLCollector - skipping ' . $this->dsn[$i] . ': ' . $e->getMessage());
-				// Error connecting to i-th database
-				continue;
-			}
-			break;
-		}
-		if ($this->db == NULL) {
-			throw new SimpleSAML_Error_Exception('attributecollector:SQLCollector - cannot connect to any database');
-		}
-		$this->db->setAttribute(PDO::ATTR_CASE, $this->attrcase);
-		return $this->db;
-	}
+        $case_options = array("lower" => PDO::CASE_LOWER,
+            "natural" => PDO::CASE_NATURAL,
+            "upper" => PDO::CASE_UPPER);
+        // Default is 'natural'
+        $this->attrcase = $case_options["natural"];
+        if (array_key_exists("attrcase", $config)) {
+            $attrcase = $config["attrcase"];
+            if (in_array($attrcase, array_keys($case_options))) {
+                $this->attrcase = $case_options[$attrcase];
+            } else {
+                throw new Exception("attributecollector:SQLCollector - Wrong case value: '" . $attrcase . "'");
+            }
+        }
+    }
 
 
-	/* Get All entries
-	 *
-	 * @return array  entries collected
-	 */
-	public function getAll($uidfield) {
-		$entries = array();        
+    /* Get collected attributes
+     *
+     * @param array $originalAttributes      Original attributes existing before this collector has been called
+     * @param string $uidfield      Name of the field used as uid
+     * @return array  Attributes collected
+     */
+    public function getAttributes($originalAttributes, $uidfield)
+    {
+        assert('array_key_exists($uidfield, $originalAttributes)');
+        $db = $this->getDB();
+        $st = $db->prepare($this->query[$this->current]);
+        if (false === $st) {
+            $err = $st->errorInfo();
+            $err_msg = 'attributecollector:SQLCollector - invalid query';
+            if (isset($err[2])) {
+                $err_msg .= ': '.$err[2];
+            }
+            throw new SimpleSAML_Error_Exception('attributecollector:SQLCollector - invalid query: '.$err[2]);
+        }
 
-		$db = $this->getDB();
+        $res = $st->execute(array(':uidfield' => $originalAttributes[$uidfield][0]));
 
-		$query = $this->getAllQuery[$this->current];
+        if (false === $res) {
+            $err = $st->errorInfo();
+            $err_msg = 'attributecollector:SQLCollector - invalid query execution';
 
-		$st = $db->prepare($query);
-		if (FALSE === $st) {
-			$err = $st->errorInfo();
-			$err_msg = 'attributecollector:SQLCollector - invalid query';
-			if (isset($err[2])) {
-				$err_msg .= ': '.$err[2];
-			}
-			throw new SimpleSAML_Error_Exception('attributecollector:SQLCollector - invalid query: '.$err[2]);
-		}
+            if (isset($err[2])) {
+                $err_msg .= ': '.$err[2];
+            } elseif (isset($err[0])) {
+                $err_msg .= ': SQLSTATE['.$err[0].']';
+            }
+            throw new SimpleSAML_Error_Exception($err_msg);
+        }
 
-		$res = $st->execute(array(':uidfield' => 'x'));
+        $db_res = $st->fetchAll(PDO::FETCH_ASSOC);
 
-		if (FALSE === $res){
-			$err = $st->errorInfo();
-			$err_msg = 'attributecollector:SQLCollector - invalid query execution';
+        $result = array();
+        foreach ($db_res as $tuple) {
+            foreach ($tuple as $colum => $value) {
+                $result[$colum][] = $value;
+            }
+        }
+        foreach ($result as $colum => $data) {
+            $result[$colum] = array_unique($data);
+        }
 
-			if (isset($err[2])) {
-				$err_msg .= ': '.$err[2];
-			}
-			else if (isset($err[0])) {
-				$err_msg .= ': SQLSTATE['.$err[0].']';
-			}
-			throw new SimpleSAML_Error_Exception($err_msg);
-		}
+        return $result;
+    }
 
-		$db_res = $st->fetchAll(PDO::FETCH_ASSOC);
 
-		foreach($db_res as $entry) {
-			$info = array();
-			foreach($entry as $colum => $value) {
-				$info[$colum][] = $value;
-			}
-			foreach($info as $colum => $data) {
-				$info[$colum] = array_unique($data);
-			}
-			if (isset($info[$uidfield]) && !empty($info[$uidfield][0])) {
-				$id = $info[$uidfield][0];
-				$entries[$id] = $info;
-			}
-			else {
-				$entries[] = $info;
-			}
-		}
+    /**
+     * Get database handle.
+     *
+     * @return PDO|FALSE  Database handle, or FALSE if we fail to connect.
+     */
+    private function getDB()
+    {
+        if ($this->db !== null) {
+            return $this->db;
+        }
 
-		return $entries;
-	}
+        for ($i = 0; $i<$this->total; $i++) {
+            $this->current = $i;
+            try {
+                $this->db = new PDO($this->dsn[$i], $this->username[$i], $this->password[$i]);
+            } catch (PDOException $e) {
+                SimpleSAML\Logger::error('attributecollector:SQLCollector - skipping ' . $this->dsn[$i] . ': ' . $e->getMessage());
+                // Error connecting to i-th database
+                continue;
+            }
+            break;
+        }
+        if ($this->db == null) {
+            throw new SimpleSAML_Error_Exception('attributecollector:SQLCollector - cannot connect to any database');
+        }
+        $this->db->setAttribute(PDO::ATTR_CASE, $this->attrcase);
+        return $this->db;
+    }
 
+
+    /* Get All entries
+     *
+     * @return array  entries collected
+     */
+    public function getAll($uidfield)
+    {
+        $entries = array();
+
+        $db = $this->getDB();
+
+        $query = $this->getAllQuery[$this->current];
+
+        $st = $db->prepare($query);
+        if (false === $st) {
+            $err = $st->errorInfo();
+            $err_msg = 'attributecollector:SQLCollector - invalid query';
+            if (isset($err[2])) {
+                $err_msg .= ': '.$err[2];
+            }
+            throw new SimpleSAML_Error_Exception('attributecollector:SQLCollector - invalid query: '.$err[2]);
+        }
+
+        $res = $st->execute(array(':uidfield' => 'x'));
+
+        if (false === $res) {
+            $err = $st->errorInfo();
+            $err_msg = 'attributecollector:SQLCollector - invalid query execution';
+
+            if (isset($err[2])) {
+                $err_msg .= ': '.$err[2];
+            } elseif (isset($err[0])) {
+                $err_msg .= ': SQLSTATE['.$err[0].']';
+            }
+            throw new SimpleSAML_Error_Exception($err_msg);
+        }
+
+        $db_res = $st->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach ($db_res as $entry) {
+            $info = array();
+            foreach ($entry as $colum => $value) {
+                $info[$colum][] = $value;
+            }
+            foreach ($info as $colum => $data) {
+                $info[$colum] = array_unique($data);
+            }
+            if (isset($info[$uidfield]) && !empty($info[$uidfield][0])) {
+                $id = $info[$uidfield][0];
+                $entries[$id] = $info;
+            } else {
+                $entries[] = $info;
+            }
+        }
+
+        return $entries;
+    }
 }
-
-?>
